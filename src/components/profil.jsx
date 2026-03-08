057d1e86b090936a9aa9537418bd73100a8a6b31
diabvass <vassiriki.diabate@epitech.eu>
2026-03-08 00:38:50 +0000

    mise en forme

--- src/components/profil.jsx
+++ src/components/profil.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import style from "./profil.module.css";
 
 export default function Profil({ nom }) {
   const [data, setData] = useState(null);
@@ -47,26 +48,43 @@ export default function Profil({ nom }) {
   const { avatar_url, name, bio, location, public_repos, followers, following, created_at, html_url } = data;
 
   return (
-    <div>
-      <img src={avatar_url} alt={name || "Avatar"} width="100" />
-      <h2>{name || data.login}</h2>
-      <p>{bio || "Pas de bio"}</p>
-      <p>Pays : {location || "Non spécifié"}</p>
-      <p>Repos : {public_repos}</p>
-      <p>Followers : {followers}</p>
-      <p>Following : {following}</p>
-      <p>Créé le : {new Date(created_at).toLocaleDateString()}</p>
-      <a href={html_url} target="_blank" rel="noopener noreferrer">Profil GitHub</a>
-
-      {recentdata && (
-        <div>
-            <p>Nom :{recentdata.name}</p>
-            <p>URL : <a href={recentdata.html_url}>{recentdata.html_url}</a></p>
-            <p>Description : {recentdata.description}</p>
-            <p>Langage principal :{recentdata.language}</p>
-            <p>Dernière mise à jour : {recentdata.updated_at}</p>
+    <div className={style.profileCard}>
+        <div className="profile-image-container">
+            <div className="profile-image-circle">
+                <i className="fab fa-github github-icon"></i>
+            </div>
         </div>
-      )}
+
+        <div className={style.userHeader}>
+            <img src={avatar_url} alt="Avatar de ${nom}" class={style.img} width="100"/>
+            <div>
+                <h2 className={style.username}>{name || data.login}</h2>
+                <p className={style.description}>{bio || "Pas de bio"}</p>
+            </div>
+        </div>
+
+        <hr className={style.ligne} aria-hidden="true" />
+        <div className={style.userDetails}>
+            <p><strong>Local :</strong> <span className={style.value}>{location || "Non spécifié"}</span></p>
+            <p><strong>Répertoire Publics :</strong> <span className={style.value}>{public_repos}</span></p>
+            <p><strong>Followers :</strong> <span className={style.value}>{followers}</span></p>
+            <p><strong>Followings :</strong> <span className={style.value}>{following}</span></p>
+            <p><strong>Compte créé le :</strong> <span className={style.value}>{new Date(created_at).toLocaleDateString()}</span></p>
+            <p><strong>Url profil :</strong> <span className={style.value}><a href={html_url} target="_blank" rel="noopener noreferrer">Profil GitHub</a></span></p>
+        </div>
+        <hr className={style.ligne} aria-hidden="true" />
+
+        <div className={style.recent}>
+            <p className={style.recenTitle}><u>Information sur le repertoire récent</u></p>
+            <div className={style.repoDetails}>
+                <p><strong>Nom :</strong> <span className={style.value}>{recentdata.name}</span></p>
+                <p><strong>URL :</strong> <span className={style.value}><a href={recentdata.html_url}>{recentdata.html_url}</a></span></p>
+                <p><strong>Description :</strong> <span className={style.value}>{recentdata.description}</span></p>
+                <p><strong>Langage principal :</strong> <span className={style.value}>{recentdata.language}</span></p>
+                <p><strong>Dernière mise à jour :</strong> <span className={style.value}>{recentdata.updated_at}</span></p>
+            </div>
+        </div>
     </div>
+
   );
 }
